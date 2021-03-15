de053f35ada99a3cee5a73acccc37e651d440d55
yuhueilee <ylee0026@student.monash.edu>
2021-03-15 09:11:21 +0800

    Section3: Effects (part 3)

--- src/app/auth/auth.effects.ts
+++ src/app/auth/auth.effects.ts
@@ -1,22 +1,23 @@
 import { User } from './model/user.model';
 import { Injectable } from "@angular/core";
-import { Actions, ofType } from "@ngrx/effects";
+import { Actions, createEffect, ofType } from "@ngrx/effects";
 import { AuthActions } from './action-types';
 import { tap } from 'rxjs/operators';
 
 @Injectable()
 export class AuthEffects {
 
-    constructor(private actions$: Actions) {
-
-        const login$ = this.actions$.pipe(
+    login$ = createEffect(() => 
+        this.actions$.pipe(
             ofType(AuthActions.login), // similar to filter the type
-            tap(action => { // create side effects
-                localStorage
-                    .setItem('user', JSON.stringify(action.user));
-            })
-        );
-        login$.subscribe();
+            tap(action => localStorage
+                    .setItem('user', JSON.stringify(action.user))
+            )
+        )
+    , {dispatch: false} // to indicate that this effect does not result in a dispatch of action
+    );
+
+    constructor(private actions$: Actions) {
     }
 
 }
