2fcf2e0ad6bca1c74b25e711c8f47c6f6fff2b25
yuhueilee <ylee0026@student.monash.edu>
2021-03-22 09:34:32 +0800

    Ngrx metaReducer for login

--- src/app/reducers/index.ts
+++ src/app/reducers/index.ts
@@ -16,5 +16,15 @@ export const reducers: ActionReducerMap<AppState> = {
   router: routerReducer
 };
 
+export function logger(reducer: ActionReducer<any>)
+  : ActionReducer<any> {
+  return (state, action) => {
+    console.log('state before: ', state);
+    console.log('action: ', action);
 
-export const metaReducers: MetaReducer<AppState>[] = !environment.production ? [] : [];
+    return reducer(state, action);
+  }
+}
+
+export const metaReducers: MetaReducer<AppState>[] = 
+  !environment.production ? [logger] : [];
