73c34639edf58c585e0d4dfd43909ac9cd63f773
yuhueilee <ylee0026@student.monash.edu>
2021-04-12 12:34:15 +0800

    Ngrx Entity: create action to update course

--- src/app/courses/courses.actions.ts
+++ src/app/courses/courses.actions.ts
@@ -1,3 +1,4 @@
+import { Update } from "@ngrx/entity";
 import { createAction, props } from "@ngrx/store";
 import { Course } from "./model/course";
 
@@ -9,4 +10,9 @@ export const loadAllCourses = createAction(
 export const allCoursesLoaded = createAction(
     '[Load Courses Effect] All Courses Loaded',
     props<{courses: Course[]}>()
+);
+
+export const courseUpdated = createAction(
+    '[Edit Course Dialog] Course Updated',
+    props<{update: Update<Course>}>()
 );
--- src/app/courses/edit-course-dialog/edit-course-dialog.component.ts
+++ src/app/courses/edit-course-dialog/edit-course-dialog.component.ts
@@ -1,9 +1,13 @@
+import { courseUpdated } from './../courses.actions';
 import {Component, Inject} from '@angular/core';
 import {MAT_DIALOG_DATA, MatDialogRef} from '@angular/material/dialog';
 import {Course} from '../model/course';
 import {FormBuilder, FormGroup, Validators} from '@angular/forms';
 import {Observable} from 'rxjs';
 import {CoursesHttpService} from '../services/courses-http.service';
+import { AppState } from '../../reducers';
+import { Store } from '@ngrx/store';
+import { Update } from '@ngrx/entity';
 
 @Component({
   selector: 'course-dialog',
@@ -26,7 +30,7 @@ export class EditCourseDialogComponent {
     private fb: FormBuilder,
     private dialogRef: MatDialogRef<EditCourseDialogComponent>,
     @Inject(MAT_DIALOG_DATA) data,
-    private coursesService: CoursesHttpService) {
+    private store: Store<AppState>) {
 
     this.dialogTitle = data.dialogTitle;
     this.course = data.course;
@@ -63,11 +67,14 @@ export class EditCourseDialogComponent {
       ...this.form.value
     };
 
-    this.coursesService.saveCourse(course.id, course)
-      .subscribe(
-        () => this.dialogRef.close()
-      )
+    const update: Update<Course> = {
+      id: course.id,
+      changes: course
+    };
 
+    this.store.dispatch(courseUpdated({update}));
+
+    this.dialogRef.close();
 
   }
 
