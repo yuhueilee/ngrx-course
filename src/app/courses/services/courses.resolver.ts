ea1ff7956b2f4d461435aaa11ca1c1684bcdb096
yuhueilee <ylee0026@student.monash.edu>
2021-05-12 14:55:41 +0800

    Ngrx data: Use loaded$ stream to only call the api once

--- src/app/courses/services/courses.resolver.ts
+++ src/app/courses/services/courses.resolver.ts
@@ -1,3 +1,4 @@
+import { filter, tap, first } from 'rxjs/operators';
 import { map } from 'rxjs/operators';
 import { CourseEntityService } from './course-entity.service';
 import { Observable } from 'rxjs';
@@ -14,8 +15,14 @@ export class CoursesResolver implements Resolve<boolean> {
         route: ActivatedRouteSnapshot,
         state: RouterStateSnapshot
     ): Observable<boolean> {
-        return this.coursesService.getAll().pipe(
-            map(courses => !!courses)
+        return this.coursesService.loaded$.pipe(
+            tap(loaded => { // perform side effect
+                if (!loaded) { // to ensure the api is being called ONCE
+                    this.coursesService.getAll();
+                }
+            }),
+            filter(loaded => !!loaded), // ensure we wait for the data to be loaded in store
+            first() // to complete the observable and ensure the transition goes through
         );
     }
 }
